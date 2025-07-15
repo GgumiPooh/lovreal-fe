a506108e9f2ae1b9299a860f3170729addac1c3b
hyo0joo <hyoeun.jin2@gmail.com>
2025-07-16 00:10:23 +0900

    Input, Label css수정

--- src/routes/Input.tsx
+++ src/routes/Input.tsx
@@ -5,15 +5,17 @@ type Props = {
     id?: string;
     className?: string;
     placeholder?: string
+    type?: string
 }
 
-function Input({name, id, className, placeholder} : Props) {
+function Input({name, id, className, placeholder, type} : Props) {
     return(
         <input
         name={name}
         id={id}
-        className={twMerge(className, "font-signup rounded-2xl, hover:border-red-800")}
+        className={twMerge(className, "outline-none rounded-2xl py-1.5 pl-2 pr-4 border-2 placeholder-gray-400 border-red-200 focus:border-red-600 hover:border-red-400 transition duration-300 ease-in-out")}
         placeholder={placeholder}
+        type={type}
         />
     )
 }
--- src/routes/Label.tsx
+++ src/routes/Label.tsx
@@ -1,18 +1,22 @@
 import { twMerge } from "tailwind-merge";
 
 type Props = {
-    className?: string;
-    htmlFor?: string;
-    children?: string
+  className?: string;
+  htmlFor?: string;
+  children?: string;
+};
+
+function Label({ className, htmlFor, children }: Props) {
+  return (
+    <label
+      className={twMerge(
+        className,
+        "font-signup text-end text-gray-500 text-[0.9rem] p-[0.3rem] w-[7rem]")}
+      htmlFor={htmlFor}
+    >
+      {children}
+    </label>
+  );
 }
 
-function Label({className, htmlFor, children} : Props) {
-    return(
-        <label
-        className={twMerge(className, "font-signup")}
-        htmlFor={htmlFor}
-        >{children}</label>
-    )
-}
-
-export default Label
+export default Label;
