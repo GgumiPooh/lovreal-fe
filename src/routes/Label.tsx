9b8d0a20b4006c157ffa92d6e6550dc68e9e8c3f
hyo0joo <hyoeun.jin2@gmail.com>
2025-07-15 22:45:46 +0900

    label 컴포넌트 생성

---
+++ src/routes/Label.tsx
@@ -0,0 +1,18 @@
+import { twMerge } from "tailwind-merge";
+
+type Props = {
+    className?: string;
+    htmlFor?: string;
+    children?: string
+}
+
+function Label({className, htmlFor, children} : Props) {
+    return(
+        <label
+        className={twMerge(className, "")}
+        htmlFor={htmlFor}
+        >{children}</label>
+    )
+}
+
+export default Label
